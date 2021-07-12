7445a0b128aec2d217e7e5cb8b40a8c449758646
elbiesmith <elbie.smith@gmail.com>
2021-07-12 15:29:56 -0500

    initial commit

---
+++ js/site.js
@@ -0,0 +1,7 @@
+
+
+function getNumbers() {
+    let startValue = parseInt(document.getElementById("startValue").value);
+    let endValue = parseInt(document.getElementById("endValue").value);
+
+}
